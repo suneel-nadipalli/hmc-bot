daac2a264d8badb359ec2c58f04ae9c9cd8c0439
suneel-nadipalli <suneel.nadipalli@duke.edu>
2025-05-09 13:45:01 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+intents.members = True
+
+herbie = commands.Bot(command_prefix='/', intents=intents)
+
+@herbie.event
+async def on_ready():
+    print(f'We have logged in as {herbie.user}')
+    print('------')
+
+@herbie.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+herbie.run(token, log_handler=handler, log_level=logging.DEBUG)
